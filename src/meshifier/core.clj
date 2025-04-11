59b211db907b741b13ccc9d326ae8201049dbd4c
dcw <-->
2025-04-10 20:06:26 -0400

    Added example function for a tetrahedron

--- src/meshifier/core.clj
+++ src/meshifier/core.clj
@@ -1,4 +1,21 @@
 (ns meshifier.core
   (:gen-class))
 
+(defn tetrahedron-mesh
+  "Returns mesh data for a tetrahedron in a format compatible with Blender's Mesh.from_pydata().
+   Returns a map containing :vertices and :faces where:
+   - :vertices is a sequence of [x y z] coordinates for each vertex
+   - :faces is a sequence of vertex indices forming each triangular face"
+  []
+  (let [vertices [[1.0 1.0 1.0]     ; vertex 0
+                  [-1.0 -1.0 1.0]    ; vertex 1
+                  [-1.0 1.0 -1.0]    ; vertex 2
+                  [1.0 -1.0 -1.0]]   ; vertex 3
+        faces [[0 1 2]              ; face 0
+               [0 1 3]              ; face 1
+               [0 2 3]              ; face 2
+               [1 2 3]]]            ; face 3
+    {:vertices vertices
+     :faces faces}))
+
 
--- test/meshifier/core_test.clj
+++ test/meshifier/core_test.clj
@@ -2,4 +2,17 @@
   (:require [clojure.test :refer :all]
             [meshifier.core :refer :all]))
 
+(deftest tetrahedron-mesh-test
+  (testing "tetrahedron-mesh returns correct structure"
+    (let [mesh (tetrahedron-mesh)]
+      (is (map? mesh))
+      (is (contains? mesh :vertices))
+      (is (contains? mesh :faces))
+      (is (= 4 (count (:vertices mesh))))
+      (is (= 4 (count (:faces mesh))))
+      (is (every? #(= 3 (count %)) (:vertices mesh)))
+      (is (every? #(= 3 (count %)) (:faces mesh)))
+      (is (every? number? (flatten (:vertices mesh))))
+      (is (every? int? (flatten (:faces mesh)))))))
+
 
