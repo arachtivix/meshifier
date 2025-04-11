2286a7b750da3466d3bcd5e398433a03c36079d9
dcw <-->
2025-04-10 22:20:32 -0400

    utility function for tetrahedrons

--- src/meshifier/core.clj
+++ src/meshifier/core.clj
@@ -47,6 +47,38 @@
      :faces faces
      :normals vertex-normals}))
 
+(defn compute-signed-volume
+  "Compute the signed volume of a tetrahedron"
+  [p1 p2 p3 p4]
+  (let [v1 (subtract-vectors p2 p1)
+        v2 (subtract-vectors p3 p1)
+        v3 (subtract-vectors p4 p1)
+        cross-prod (cross-product v1 v2)
+        dot-prod (reduce + (map * cross-prod v3))]
+    (/ dot-prod 6.0)))
+
+(defn point-in-tetrahedron?
+  "Determines if a point p lies inside the tetrahedron defined by vertices p1, p2, p3, p4.
+   Returns true if the point is inside or on the surface of the tetrahedron, false otherwise.
+   
+   Uses the consistent sign of volumes method:
+   - Compute the signed volume of the full tetrahedron
+   - Compute the signed volumes of four tetrahedra formed by the test point and three vertices
+   - If all five volumes have the same sign (or are zero), the point is inside or on the surface"
+  [p p1 p2 p3 p4]
+  (let [total-volume (compute-signed-volume p1 p2 p3 p4)
+        d1 (compute-signed-volume p p2 p3 p4)
+        d2 (compute-signed-volume p1 p p3 p4)
+        d3 (compute-signed-volume p1 p2 p p4)
+        d4 (compute-signed-volume p1 p2 p3 p)]
+    (if (zero? total-volume)
+      false  ; Degenerate tetrahedron
+      (let [sign (if (pos? total-volume) pos? neg?)]
+        (and (or (zero? d1) (sign d1))
+             (or (zero? d2) (sign d2))
+             (or (zero? d3) (sign d3))
+             (or (zero? d4) (sign d4)))))))
+
 (defn tetrahedron-mesh
   "Returns mesh data for a regular tetrahedron with predefined vertices.
    Returns a map containing :vertices, :faces, and :normals where:
--- test/meshifier/core_test.clj
+++ test/meshifier/core_test.clj
@@ -2,6 +2,54 @@
   (:require [clojure.test :refer :all]
             [meshifier.core :refer :all]))
 
+(deftest point-in-tetrahedron-test
+  (testing "point-in-tetrahedron? with regular tetrahedron"
+    (let [p1 [1.0 1.0 1.0]      ; vertex 0
+          p2 [-1.0 -1.0 1.0]    ; vertex 1
+          p3 [-1.0 1.0 -1.0]    ; vertex 2
+          p4 [1.0 -1.0 -1.0]]   ; vertex 3
+      
+      ; Test points inside
+      (is (point-in-tetrahedron? [0.0 0.0 0.0] p1 p2 p3 p4) "Center point should be inside")
+      (is (point-in-tetrahedron? [0.5 0.5 0.5] p1 p2 p3 p4) "Point near vertex p1 should be inside")
+      
+      ; Test vertices (should be inside/on surface)
+      (is (point-in-tetrahedron? p1 p1 p2 p3 p4) "Vertex p1 should be inside")
+      (is (point-in-tetrahedron? p2 p1 p2 p3 p4) "Vertex p2 should be inside")
+      (is (point-in-tetrahedron? p3 p1 p2 p3 p4) "Vertex p3 should be inside")
+      (is (point-in-tetrahedron? p4 p1 p2 p3 p4) "Vertex p4 should be inside")
+      
+      ; Test points on faces (should be inside/on surface)
+      (is (point-in-tetrahedron? [(/ (+ (p1 0) (p2 0) (p3 0)) 3)
+                                 (/ (+ (p1 1) (p2 1) (p3 1)) 3)
+                                 (/ (+ (p1 2) (p2 2) (p3 2)) 3)]
+                                p1 p2 p3 p4)
+          "Point on face center should be inside")
+      
+      ; Test points outside
+      (is (not (point-in-tetrahedron? [2.0 2.0 2.0] p1 p2 p3 p4)) "Point far from tetrahedron should be outside")
+      (is (not (point-in-tetrahedron? [-2.0 -2.0 -2.0] p1 p2 p3 p4)) "Point far from tetrahedron should be outside")
+      
+      ; Test edge cases
+      (is (not (point-in-tetrahedron? [0.0 0.0 0.0] p1 p1 p2 p3))
+          "Degenerate tetrahedron (repeated vertex) should return false")))
+  
+  (testing "point-in-tetrahedron? with degenerate cases"
+    (let [p1 [0.0 0.0 0.0]
+          p2 [1.0 0.0 0.0]
+          p3 [0.0 1.0 0.0]
+          p4 [0.0 0.0 0.0]]  ; p4 same as p1 - degenerate case
+      (is (not (point-in-tetrahedron? [0.25 0.25 0.0] p1 p2 p3 p4))
+          "Point with degenerate tetrahedron should return false")))
+  
+  (testing "point-in-tetrahedron? with flat tetrahedron"
+    (let [p1 [0.0 0.0 0.0]
+          p2 [1.0 0.0 0.0]
+          p3 [0.0 1.0 0.0]
+          p4 [1.0 1.0 0.0]]  ; All points in same plane - degenerate case
+      (is (not (point-in-tetrahedron? [0.5 0.5 0.0] p1 p2 p3 p4))
+          "Point with flat tetrahedron should return false"))))
+
 (deftest tetrahedron-mesh-test
   (testing "tetrahedron-mesh returns correct structure"
     (let [mesh (tetrahedron-mesh)]
@@ -23,5 +71,6 @@
                      (Math/sqrt (reduce + (map (fn [x] (* x x)) %)))
                      1.001)
                   (:normals mesh))))))
+
 
 
